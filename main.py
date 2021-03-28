b58f9cb2ee990fdb4485474860a640773dfbab4d
UltraProjectPl <jakub.sumara09@gmail.com>
2021-03-28 15:44:30 +0200

    People make hungry

--- main.py
+++ main.py
@@ -1,14 +1,39 @@
 from typing import List
+from time import sleep
+from random import randint
 
 
 class Person:
-    pass
+    def __init__(self, id: int):
+        self.id: int = id
+        self.starving: int = 0
+
+    def hungry(self):
+        self.starving += randint(1, 3)
+
+
+def showPersons(persons: List[Person]):
+    for person in persons:
+        print("""
+            Person:
+               Id: {}
+               Starving: {}
+        """.format(person.id, person.starving))
 
 
 persons: List[Person] = []
-for _ in range(5):
-    persons.append(Person())
+for i in range(5):
+    persons.append(Person(i))
 
 
-for person in persons:
-    print(person)
+for _ in range(10):
+    for person in persons:
+        person.hungry()
+        if person.starving > 10:
+            persons.remove(person)
+    showPersons(persons)
+    sleep(1)
+
+
+print("===============================END==========================================")
+showPersons(persons)
